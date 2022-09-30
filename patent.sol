128297d662b903d5f716dae58c91df69599c0b87
bjd-2002 <111186404+bjd-2002@users.noreply.github.com>
2022-09-30 23:28:13 +0530

    Create patent.sol

---
+++ patent.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Patent {
+    struct sys-admin {
+        address id;
+        bytes32 name;
+
+    }
+    struct user {
+        bytes32 country;
+        bool isOwner;
+        bytes32 name;
+        uint age;
+        
+    }
+
+}
